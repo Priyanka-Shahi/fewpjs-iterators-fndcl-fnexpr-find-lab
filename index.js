5dfc0b3d920d47f734fef6da699038ee50bedf00
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-25 19:53:30 +1000

    array find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+
+  function superbowlWin(records) {
+    let found = records.find(rec=>rec.result==='W');
+    if(found) {
+        return found.year;
+    } else {
+        return undefined;
+    }
+    }
+
+console.log(superbowlWin(record));
